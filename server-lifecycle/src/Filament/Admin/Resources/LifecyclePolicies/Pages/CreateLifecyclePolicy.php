7c4c1043abde75dcac63941fb1c6d55050150118
harbourmastersam <sam@greyharbour.net>
2026-09-11 18:23:42 +0930

    fix(server-lifecycle): keep actions visible with button style

--- server-lifecycle/src/Filament/Admin/Resources/LifecyclePolicies/Pages/CreateLifecyclePolicy.php
+++ server-lifecycle/src/Filament/Admin/Resources/LifecyclePolicies/Pages/CreateLifecyclePolicy.php
@@ -2,13 +2,43 @@
 
 namespace HarbourmasterSam\ServerLifecycle\Filament\Admin\Resources\LifecyclePolicies\Pages;
 
+use App\Enums\TablerIcon;
+use App\Traits\Filament\CanCustomizeHeaderActions;
+use App\Traits\Filament\CanCustomizeHeaderWidgets;
+use Filament\Actions\Action;
+use Filament\Actions\ActionGroup;
 use Filament\Resources\Pages\CreateRecord;
 use HarbourmasterSam\ServerLifecycle\Filament\Admin\Resources\LifecyclePolicies\LifecyclePolicyResource;
 use HarbourmasterSam\ServerLifecycle\Services\Policy\DurationNormalizer;
 
 class CreateLifecyclePolicy extends CreateRecord
 {
+    use CanCustomizeHeaderActions;
+    use CanCustomizeHeaderWidgets;
+
     protected static string $resource = LifecyclePolicyResource::class;
+
+    protected static bool $canCreateAnother = false;
+
+    /** @return array<Action|ActionGroup> */
+    protected function getDefaultHeaderActions(): array
+    {
+        return [
+            Action::make('create')
+                ->hiddenLabel()
+                ->action('create')
+                ->keyBindings(['mod+s'])
+                ->tooltip(
+                    trans('filament-panels::resources/pages/create-record.form.actions.create.label')
+                )
+                ->icon(TablerIcon::FilePlus),
+        ];
+    }
+
+    protected function getFormActions(): array
+    {
+        return [];
+    }
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
--- server-lifecycle/src/Filament/Admin/Resources/LifecyclePolicies/Pages/EditLifecyclePolicy.php
+++ server-lifecycle/src/Filament/Admin/Resources/LifecyclePolicies/Pages/EditLifecyclePolicy.php
@@ -2,13 +2,44 @@
 
 namespace HarbourmasterSam\ServerLifecycle\Filament\Admin\Resources\LifecyclePolicies\Pages;
 
+use App\Enums\TablerIcon;
+use App\Traits\Filament\CanCustomizeHeaderActions;
+use App\Traits\Filament\CanCustomizeHeaderWidgets;
+use Filament\Actions\Action;
+use Filament\Actions\ActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 use HarbourmasterSam\ServerLifecycle\Filament\Admin\Resources\LifecyclePolicies\LifecyclePolicyResource;
 use HarbourmasterSam\ServerLifecycle\Services\Policy\DurationNormalizer;
 
 class EditLifecyclePolicy extends EditRecord
 {
+    use CanCustomizeHeaderActions;
+    use CanCustomizeHeaderWidgets;
+
     protected static string $resource = LifecyclePolicyResource::class;
+
+    /** @return array<Action|ActionGroup> */
+    protected function getDefaultHeaderActions(): array
+    {
+        return [
+            DeleteAction::make(),
+
+            Action::make('save')
+                ->hiddenLabel()
+                ->action('save')
+                ->keyBindings(['mod+s'])
+                ->tooltip(
+                    trans('filament-panels::resources/pages/edit-record.form.actions.save.label')
+                )
+                ->icon(TablerIcon::DeviceFloppy),
+        ];
+    }
+
+    protected function getFormActions(): array
+    {
+        return [];
+    }
 
     protected function mutateFormDataBeforeFill(array $data): array
     {
